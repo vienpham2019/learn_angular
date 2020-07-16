b992175f42af83cb267bf9eba05d1e523d4bb888
vienpham2019 <vienpham2019@gmail.com>
2020-07-16 17:58:53 -0500

    save

--- angular-app/src/app/components/test/test.component.ts
+++ angular-app/src/app/components/test/test.component.ts
@@ -4,43 +4,24 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-test',
   template: `
     <div>
-      hello {{title}}
-      <br>
-      <h2 [ngClass] = "messageClass"> This is message </h2> 
-      <h2 [class.text-success] = "success"> This is message </h2> 
+      <h2> Welcome to {{title}} </h2> 
+      <button (click) = "onClick()">Greeting</button> 
+      <button (click) = "greeting=''">Clear Greeting</button> 
+      <h2>{{greeting}}</h2> 
     </div> 
   `,
-  styles: [`
-    .text-success {
-      color: green
-    }
-
-    .text-danger {
-      color: red
-    }
-
-    .text-special {
-      font-size: 2em
-    }
-  `]
+  styles: []
 })
 export class TestComponent implements OnInit {
-  title: string = 'My first angular app.'
-  myId: string = 'testId'
-  spectial: boolean = false
-  success: boolean = false
-  messageClass = {
-    "text-danger": !this.success,
-    "text-success": this.success,
-    'text-special': this.spectial
-  }
+  title: string = "code evolution"
+  greeting: string = ""
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  greetingUser () {
-    return 'Hello new User. '
+  onClick() {
+    this.greeting = "hi new user. Welcome to code evolution"
   }
 
 }
