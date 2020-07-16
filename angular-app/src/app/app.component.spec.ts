8597cf8eb72bc29de16c0b25d252003056e54142
vienpham2019 <vienpham2019@gmail.com>
2020-07-16 16:42:41 -0500

    save

--- angular-app/src/app/app.component.spec.ts
+++ angular-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-todolist'`, () => {
+  it(`should have as title 'angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-todolist');
+    expect(app.title).toEqual('angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-todolist app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-app app is running!');
   });
 });
