7a7b73d7db5ca0de8c8bb65388600d8159b7f555
vienpham2019 <vienpham2019@gmail.com>
2020-07-16 17:41:30 -0500

    save

--- angular-app/src/app/components/test/test.component.ts
+++ angular-app/src/app/components/test/test.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-test]',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: 'app-test',
+  template: `
+    <div>
+      hello {{title}}
+      <br>
+      <h2 [ngClass] = "messageClass"> This is message </h2> 
+      <h2 [class.text-success] = "success"> This is message </h2> 
+    </div> 
+  `,
+  styles: [`
+    .text-success {
+      color: green
+    }
+
+    .text-danger {
+      color: red
+    }
+
+    .text-special {
+      font-size: 2em
+    }
+  `]
 })
 export class TestComponent implements OnInit {
-
+  title: string = 'My first angular app.'
+  myId: string = 'testId'
+  spectial: boolean = false
+  success: boolean = false
+  messageClass = {
+    "text-danger": !this.success,
+    "text-success": this.success,
+    'text-special': this.spectial
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  greetingUser () {
+    return 'Hello new User. '
   }
 
 }
