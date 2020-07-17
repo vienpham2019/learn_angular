cd2f71584b19cbe2a7f95a95b12a8a5b52698057
vienpham2019 <vienpham2019@gmail.com>
2020-07-16 23:04:01 -0500

    save

--- angular-app/src/app/components/test/test.component.ts
+++ angular-app/src/app/components/test/test.component.ts
@@ -3,17 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <div>
-      <h2> Welcome to {{title}} </h2> 
-      <h3 *ngIf= displayMessage > This is hidden ? </h3> 
-      <button (click) = "displayMessage = !displayMessage" >{{displayMessage ? 'hidden' : 'show' }}</button > 
+    <div *ngFor= "let color of colors; index as i ; odd as o ; last as l ; first as f ; event as e"> 
+      <h2> 
+        odd_index?: {{o}} ____
+        event_index?: {{e}} ____
+        is_last_index: {{l}} ____
+        is_first_index: {{f}} ____ 
+        index: {{i}} ____
+        <label [style.color] = color >color: {{color}}</label> ____
+
+      </h2> 
     </div> 
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
-  title: string = "code evolution"
-  displayMessage: boolean = true
+  colors: string[] = ['red', 'green' , 'orange', 'purple']
   constructor() { }
 
   ngOnInit(): void {
