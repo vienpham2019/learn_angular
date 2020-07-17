8360aa473fb040f0a2056116ce1a179db98532d0
vienpham2019 <vienpham2019@gmail.com>
2020-07-17 17:46:13 -0500

    save

--- angular-app/src/app/components/test/test.component.ts
+++ angular-app/src/app/components/test/test.component.ts
@@ -11,17 +11,41 @@ import { Component, OnInit } from '@angular/core';
         is_first_index: {{f}} ____ 
         index: {{i}} ____
         <label [style.color] = color >color: {{color}}</label> ____
-
       </h2> 
     </div> 
+    <label> First Name </label> 
+    <input #first_name type='text' />
+    <label> Last Name </label> 
+    <input #last_name type='text' /> 
+    <label> Age </label> 
+    <input #age type = 'text' />
+    <button (click) = "onClick(first_name.value, last_name.value , age.value)" >Submit </button> 
+    <h2 [ngClass] = "messageClass" >{{greeting}}</h2> 
   `,
-  styles: []
+  styles: [
+    `
+    .color-red {color: red}
+    .special-text {font-size: 3em}
+    `
+  ]
 })
 export class TestComponent implements OnInit {
   colors: string[] = ['red', 'green' , 'orange', 'purple']
+  greeting: string = ""
+  color_red: boolean = true
+  special_text: boolean = true 
+  messageClass = {
+    "color-red": this.color_red, 
+    "special-text": this.special_text
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick(first_name , last_name , age):void {
+    this.greeting = `Hi ${first_name} ${last_name}. Wellcome to my app, and you are ${age} years old.`
   }
 
 }
