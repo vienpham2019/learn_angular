d9ba3f86c1feaa854da14f670f8aabb2209abe76
vienpham2019 <vienpham2019@gmail.com>
2020-07-17 22:46:29 -0500

    save

--- angular-app/src/app/components/test/test.component.ts
+++ angular-app/src/app/components/test/test.component.ts
@@ -3,49 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <div *ngFor= "let color of colors; index as i ; odd as o ; last as l ; first as f ; event as e"> 
-      <h2> 
-        odd_index?: {{o}} ____
-        event_index?: {{e}} ____
-        is_last_index: {{l}} ____
-        is_first_index: {{f}} ____ 
-        index: {{i}} ____
-        <label [style.color] = color >color: {{color}}</label> ____
-      </h2> 
-    </div> 
-    <label> First Name </label> 
-    <input #first_name type='text' />
-    <label> Last Name </label> 
-    <input #last_name type='text' /> 
-    <label> Age </label> 
-    <input #age type = 'text' />
-    <button (click) = "onClick(first_name.value, last_name.value , age.value)" >Submit </button> 
-    <h2 [ngClass] = "messageClass" >{{greeting}}</h2> 
-  `,
-  styles: [
-    `
-    .color-red {color: red}
-    .special-text {font-size: 3em}
-    `
-  ]
+    <h2> {{ name | lowercase }} </h2> 
+    <h2> {{ name | uppercase }} </h2>  
+    <h2> {{ message | titlecase }} </h2> 
+    <h2> {{ name | slice:2:6 }} </h2> 
+    <h2> {{ person | json }} </h2> 
+
+    <h2> {{ myNumber | number:'3.2-4' }} </h2> 
+    <h2> {{ myNumber | number:'1.4-6' }} </h2> 
+    <h2> {{ myNumber | number:'2.1-2' }} </h2> 
+
+    <h2> {{ 0.25 | percent }} </h2> 
+    <h2> {{ 0.25 | currency }} </h2> 
+    <h2> {{ 0.25 | currency: 'GBP' }} </h2> 
+    <h2> {{ 120000 | currency: 'VND ': 'code' }} </h2> 
+
+    <h2> {{ date }} </h2> 
+    <h2> {{ date | date:'short' }} </h2> 
+
+  `, 
+  styles: []
 })
 export class TestComponent implements OnInit {
-  colors: string[] = ['red', 'green' , 'orange', 'purple']
-  greeting: string = ""
-  color_red: boolean = true
-  special_text: boolean = true 
-  messageClass = {
-    "color-red": this.color_red, 
-    "special-text": this.special_text
+  name: string = "My first angular app"
+  message: string = "Welcome to my app"
+  person: any = {
+    "first_name": "Vien",
+    "last_name": "Pham"
   }
-
+  myNumber: number = 15.331
+  date: any = new Date()
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onClick(first_name , last_name , age):void {
-    this.greeting = `Hi ${first_name} ${last_name}. Wellcome to my app, and you are ${age} years old.`
   }
 
 }
