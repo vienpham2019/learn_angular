527a0aa0065209d997972ef5e71811ad4be24162
vienpham2019 <vienpham2019@gmail.com>
2020-08-03 22:35:31 -0500

    add subscribe validation

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 // import { FormGroup, FormControl } from '@angular/forms'
-import { FormBuilder , Validators } from '@angular/forms'
+import { FormBuilder , Validators, FormGroup } from '@angular/forms'
 import { forbiddenValidation } from './shared/forbiddenValidation'
 import { passwordValidation } from './shared/passwordValidation'
 
@@ -9,23 +9,38 @@ import { passwordValidation } from './shared/passwordValidation'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  registrationForm: FormGroup
 
   constructor (private _fb: FormBuilder ) { }
 
-  registrationForm = this._fb.group({
-    userName: ["" , [Validators.required , Validators.minLength(3)]], 
-    email: ["", [Validators.required, forbiddenValidation(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]],
-    subscribe: [false], 
-    phone: ["", [Validators.required, forbiddenValidation(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)]],
-    password: ["" , [Validators.required , Validators.minLength(5)]],
-    confirmPassword: [""],
-    address: this._fb.group({
-      city: [""],
-      state: [""],
-      zipcode: [""]
-    })
-  } , {validators: passwordValidation})
+  ngOnInit(){
+    this.registrationForm = this._fb.group({
+      userName: ["" , [Validators.required , Validators.minLength(3)]], 
+      email: [""],
+      subscribe: [false], 
+      phone: ["", [Validators.required, forbiddenValidation(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)]],
+      password: ["" , [Validators.required , Validators.minLength(5)]],
+      confirmPassword: [""],
+      address: this._fb.group({
+        city: [""],
+        state: [""],
+        zipcode: [""]
+      })
+    } , {validators: passwordValidation})
+
+    this.registrationForm.get('subscribe').valueChanges
+      .subscribe(checkedValue => {
+        let email = this.registrationForm.get('email')
+        if(checkedValue){
+          email.setValidators([Validators.required, forbiddenValidation(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)])
+        }else{
+          email.clearValidators()
+        }
+        email.updateValueAndValidity()
+      })
+  } 
 
   loadApiData(){
     this.registrationForm.setValue({
