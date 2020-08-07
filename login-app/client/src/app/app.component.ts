d2ac00569d7ca4c3cc4106d07508da5e63ef629c
vienpham2019 <vienpham2019@gmail.com>
2020-08-06 22:34:51 -0500

    add new reactive form

--- login-app/client/src/app/app.component.ts
+++ login-app/client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { FormGroup , FormBuilder , Validators , FormArray } from '@angular/forms' 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  registrationForm: FormGroup
+
+  constructor(private _fb: FormBuilder){}
+
+  ngOnInit(){ 
+    this.registrationForm = this._fb.group({
+      userName: [''],
+      email: [''],
+      password: [''],
+      confirmPassword: ['']
+    })
+  }
 }
